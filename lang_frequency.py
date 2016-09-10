139ef1464e008c499ef26c9b8f3036702934f067
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-09-10 12:25:03 +0300

    добавлено описание функций

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,21 @@
+import collections
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        file_text = file.read()
+    return file_text
 
 
 def get_most_frequent_words(text):
-    pass
-
+    word_frequency = {}
+    words = []
+    words = re.findall('\w+', text.lower())
+    word_frequency = collections.Counter(words).most_common(10)
+    print ("10 наиболее часто встречающихся слов:")
+    for word in word_frequency:
+        print (("%s - %s повторений") % (word[0], word[1]))
 
 if __name__ == '__main__':
-    pass
+    get_most_frequent_words(load_data('file.txt'))
